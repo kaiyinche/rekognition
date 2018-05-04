7de5b6f87600bc1b879b7dd845b06b0a8359d759
Ubuntu <ubuntu@ip-192-168-1-84.ap-northeast-2.compute.internal>
2018-05-04 02:05:54 +0000

    init commit

---
+++ test.py
@@ -0,0 +1,16 @@
+import boto3
+
+if __name__ == "__main__":
+
+    imageFile='IMG_0014.PNG'
+    client=boto3.client('rekognition','eu-west-1')
+   
+    with open(imageFile, 'rb') as image:
+        response = client.detect_labels(Image={'Bytes': image.read()})
+        
+    print('Detected labels in ' + imageFile)    
+    for label in response['Labels']:
+        print (label['Name'] + ' : ' + str(label['Confidence']))
+
+    print('Done...')
+
